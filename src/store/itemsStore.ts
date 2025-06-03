bcb07db1efcbcc00561eb36a288b4d0fba46cf38
0xsingh <idlejatt@gmail.com>
2025-06-03 06:07:02 +0530

    rename at any depth

--- src/store/itemsStore.ts
+++ src/store/itemsStore.ts
@@ -9,17 +9,30 @@ export type IFlattenedItem = IItem & { ancestorIds: string[] }
 
 interface IItemsStore {
   items: IItem[]
+  addItem: (item: IItem) => void
   setItems: (items: IItem[]) => void
   updateItem: (id: string, data: Partial<IItem>) => void
 }
 
+function updateRecursive(items: IItem[], id: string, data: Partial<IItem>) {
+  return items.map((item): IItem => {
+    if (item.id === id) {
+      return { ...item, ...data }
+    }
+    if (item.children?.length && item.id !== id) {
+      return {...item, children: updateRecursive(item.children, id, data)}
+    }
+    return item
+  })
+}
+
 export const useItemsStore = create<IItemsStore>((set, get) => ({
   items: [],
+  addItem: (item) => {
+    return
+  },
   setItems: (items) => set({ items }),
   updateItem: (id, data) => {
-    const updated = get().items.map((item) =>
-      item.id === id ? { ...item, ...data } : item
-    )
-    get().setItems(updated)
+    get().setItems(updateRecursive(get().items, id, data))
   },
 }))
